4af1b0456c077987d69247f4b20232df8db4da34
AdeliaPutrii1 <55269297+AdeliaPNA1@users.noreply.github.com>
2022-06-12 23:52:29 +0700

    Add files via upload

---
+++ CC/Node JS/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var verifyToken = require('./middleware/verifyToken')
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/',verifyToken, indexRouter);
+
+
+module.exports = app;
